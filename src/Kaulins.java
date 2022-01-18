634cb52568ba84d50d8a055fec7049d28cafddd7
Cernisevs <kristers.cernisevs@gmail.com>
2022-01-18 10:45:13 +0200

    Papildinats ar ciklu

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -1,12 +1,18 @@
 import java.util.Random;
 
 public class Kaulins {
-
-	public static void main(String[] args) {
+	
+	static void mestKaulinu(int reizes){
 		Random rand = new Random();
-		int skaitlis = rand.nextInt(6)+1;
+		int skaitlis;
+		for(int i=0; i<=reizes; i++){
+			skaitlis = rand.nextInt(6)+1;
+	
 		System.out.println("Uzkrita : "+skaitlis);
-
+	}
+	}
+	public static void main(String[] args) {
+	mestKaulinu(3);
 	}
 
 }
